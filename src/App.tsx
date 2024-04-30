23f7d0ff1a431a75808c40751f1f38028817732f
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-04-30 14:02:41 +0200

    🚧 add basic card to test forecast

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,48 @@ import './App.css'
 import { SearchBox, Loader } from './components'
 import { ForwardGeocode } from './utilities/Geocode'
 import type { GeocodeResult } from './utilities/Geocode'
+import type { Forecast } from './types/forecast-types'
 import { getForecast } from './utilities/Forecast'
 
 function App() {
   const [location, setLocation] = useState<GeocodeResult | false>(false)
+  const [forecast, setForecast] = useState<Forecast | undefined>(undefined)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     setLoading(false)
-  }, [location])
+  }, [forecast])
+
+  const handleSearchSubmit = async (location: string) => {
+    setLoading(true)
+
+    const geocoded = await ForwardGeocode(location)
+    if (!geocoded) return false
+
+    const forecast = await getForecast(geocoded)
+    if (!forecast) return false
+
+    setLocation(geocoded)
+    setForecast(forecast)
+  }
 
   return (
     <div className="pk-4 container mx-auto">
-      <SearchBox
-        handleSubmit={async (location: string) => {
-          setLoading(true)
-          const geocoded = await ForwardGeocode(location)
-          if (geocoded) {
-            const forecast = await getForecast(geocoded)
-            console.log(forecast)
-          }
-          setLocation(geocoded)
-        }}
-      />
+      <SearchBox handleSubmit={handleSearchSubmit} />
 
       {loading && <Loader />}
+
+      {!loading && location && forecast && (
+        <div className="mt-6 max-w-sm overflow-hidden rounded border-2 border-teal-500">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">{location.formatted}</div>
+            <p className="text-base text-gray-700">
+              {forecast.properties.timeseries[0].data.instant.details.air_temperature}{' '}
+              {forecast.properties.meta.units.air_temperature}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
