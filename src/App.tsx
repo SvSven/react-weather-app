2b977441d872a0dd1b023a1ff5678a7989417bad
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-04-30 13:37:41 +0200

    🚧 add temp search component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { SearchBox, Loader } from './components'
+import { ForwardGeocode } from './utilities/Geocode'
+import type { GeocodeResult } from './utilities/Geocode'
+import { getForecast } from './utilities/Forecast'
 
 function App() {
-  return <h1>Hello world</h1>
+  const [location, setLocation] = useState<GeocodeResult | false>(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [location])
+
+  return (
+    <div className="pk-4 container mx-auto">
+      <SearchBox
+        handleSubmit={async (location: string) => {
+          setLoading(true)
+          const geocoded = await ForwardGeocode(location)
+          if (geocoded) {
+            const forecast = await getForecast(geocoded)
+            console.log(forecast)
+          }
+          setLocation(geocoded)
+        }}
+      />
+
+      {loading && <Loader />}
+    </div>
+  )
 }
 
 export default App
