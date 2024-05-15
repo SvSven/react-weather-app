f5dd3dcf519d566c4df5ffe6453f4e2fb5032649
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-05-15 11:54:29 +0200

    🚧 add upcoming forecast to weathercard

--- src/components/Forecast/WeatherCard.tsx
+++ src/components/Forecast/WeatherCard.tsx
@@ -1,29 +1,32 @@
 import { getImageURL } from '../../utilities/image-utils'
-import { getIconDescription } from '../../utilities/Forecast'
+import { getIconDescription, filterTimeseries } from '../../utilities/Forecast'
+import { Upcoming } from './Upcoming'
 import type { Forecast } from '../../types/forecast-types'
 import type { GeocodeResult } from '../../utilities/Geocode'
 
 export const WeatherCard = ({ forecast, location }: { forecast: Forecast; location: GeocodeResult }) => {
-  const symbolCode = forecast.properties.timeseries[0].data.next_1_hours.summary.symbol_code
+  const timeseries = filterTimeseries(forecast.properties.timeseries)
+  const currentForecast = timeseries[0]
+
+  const symbolCode = currentForecast.data.next_1_hours.summary.symbol_code
   const description = getIconDescription(symbolCode)
 
   return (
-    <div className="mt-6 max-w-sm overflow-hidden rounded bg-gradient-to-r from-sky-700 via-cyan-700 to-sky-800">
-      <div className="p-6 text-white">
+    <div className="mt-6 max-w-sm overflow-hidden rounded bg-gradient-to-r from-sky-700 via-cyan-700 to-sky-800 text-white">
+      <div className="p-6">
         <h3 className="text-base font-light">{location.formatted}</h3>
-        <div className="flex pt-4">
+        <div className="flex gap-4 pt-4">
+          <p className="text-left text-5xl font-semibold">
+            {currentForecast.data.instant.details.air_temperature}
+            <span className="ml-1 align-top text-2xl">°C</span>
+          </p>
           <div className="flex flex-grow items-center gap-2">
             <img src={getImageURL(symbolCode)} alt={description} className="w-full max-w-12 object-cover" />
             <p className="text-lg font-semibold">{description}</p>
           </div>
-          <div>
-            <p className="text-right text-5xl font-semibold">
-              {forecast.properties.timeseries[0].data.instant.details.air_temperature}
-              <span className="ml-1 align-top text-2xl">°C</span>
-            </p>
-          </div>
         </div>
       </div>
+      <Upcoming timeseries={timeseries.slice(1, 5)} />
     </div>
   )
 }
